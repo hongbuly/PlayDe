af0e843be5d6c1bb1cc0c8591adfc9ce1d7d8671
hongbuly <60245930+hongbuly@users.noreply.github.com>
2021-07-05 00:53:49 +0900

    후기 화면 만들고 프로필 클릭했을 때 연결되는 것만 함.

--- app/src/main/java/com/example/play_de/MainActivity.java
+++ app/src/main/java/com/example/play_de/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.play_de;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +42,36 @@ public class MainActivity extends AppCompatActivity {
         images.add(R.drawable.chat);
         images.add(R.drawable.user);
 
-        for(int i=0; i<5; i++)
+        for (int i = 0; i < 5; i++)
             tab.getTabAt(i).setIcon(images.get(i));
+
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProfileFragment.back_view();
+            }
+        });
+    }
+
+    private long backKeyPressedTime = 0;
+    private Toast toast;
+
+    @Override
+    public void onBackPressed() {
+        if (ProfileFragment.is_review_view()) {
+            ProfileFragment.back_view();
+        } else {
+            if (System.currentTimeMillis() > backKeyPressedTime + 2500) {
+                backKeyPressedTime = System.currentTimeMillis();
+                toast = Toast.makeText(this, "뒤로 가기 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_LONG);
+                toast.show();
+                return;
+            }
+
+            if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
+                finish();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/play_de/ProfileFragment.java
+++ app/src/main/java/com/example/play_de/ProfileFragment.java
@@ -7,10 +7,54 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    public static LinearLayout main_profile;
+
+    private ImageView profile_image;
+    private TextView profile_name;
+    private ImageButton heart_btn;
+    private ImageButton store_btn;
+
+    private Button recent_play_btn;
+    private LinearLayout recent_play01M;
+    private ImageView recent_play_image01M;
+    private TextView recent_play_name01M;
+    private LinearLayout recent_play02M;
+    private ImageView recent_play_image02M;
+    private TextView recent_play_name02M;
+    private LinearLayout recent_play03M;
+    private ImageView recent_play_image03M;
+    private TextView recent_play_name03M;
+    private LinearLayout recent_play04M;
+    private ImageView recent_play_image04M;
+    private TextView recent_play_name04M;
+
+    private Button friend_btn;
+    private LinearLayout friend01;
+    private ImageView friend_image01;
+    private TextView friend_name01;
+    private LinearLayout friend02;
+    private ImageView friend_image02;
+    private TextView friend_name02;
+    private LinearLayout friend03;
+    private ImageView friend_image03;
+    private TextView friend_name03;
+    private LinearLayout friend04;
+    private ImageView friend_image04;
+    private TextView friend_name04;
+
+
+    public static LinearLayout review_profile;
+    private LinearLayout review_background;
 
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
@@ -30,6 +74,64 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //프로필 화면 자바 코드 작성을 여기에.
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        main_profile = view.findViewById(R.id.main_profile);
+        review_profile = view.findViewById(R.id.review_profile);
+        review_background = view.findViewById(R.id.review_background);
+
+        recent_play01M = view.findViewById(R.id.recent_play01M);
+        recent_play01M.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                main_profile.setVisibility(View.GONE);
+                review_profile.setVisibility(View.VISIBLE);
+                review_background.setBackgroundResource(R.drawable.word_balloon01);
+            }
+        });
+
+        recent_play02M = view.findViewById(R.id.recent_play02M);
+        recent_play02M.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                main_profile.setVisibility(View.GONE);
+                review_profile.setVisibility(View.VISIBLE);
+                review_background.setBackgroundResource(R.drawable.word_balloon02);
+            }
+        });
+
+        recent_play03M = view.findViewById(R.id.recent_play03M);
+        recent_play03M.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                main_profile.setVisibility(View.GONE);
+                review_profile.setVisibility(View.VISIBLE);
+                review_background.setBackgroundResource(R.drawable.word_balloon03);
+            }
+        });
+
+        recent_play04M = view.findViewById(R.id.recent_play04M);
+        recent_play04M.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                main_profile.setVisibility(View.GONE);
+                review_profile.setVisibility(View.VISIBLE);
+                review_background.setBackgroundResource(R.drawable.word_balloon04);
+            }
+        });
+
+        return view;
+    }
+
+    public static void back_view() {
+        main_profile.setVisibility(View.VISIBLE);
+        review_profile.setVisibility(View.GONE);
+    }
+
+    public static boolean is_review_view() {
+        if (main_profile.getVisibility() == View.GONE) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
