96ae28e14b93a1202c8ec5ab11c9a4926649fed8
hongbuly <60245930+hongbuly@users.noreply.github.com>
2021-07-13 21:28:34 +0900

    community 버튼 애니메이션을 만들었으나, 부자연스러움.

--- app/src/main/java/com/example/play_de/CommunityFragment.java
+++ app/src/main/java/com/example/play_de/CommunityFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -26,6 +28,8 @@ public class CommunityFragment extends Fragment {
     private Button filterBtn02_1;
     private Button filterBtn02_2;
     private Button filterBtn02_3;
+    private View line01;
+    private View line02;
 
     public static CommunityFragment newInstance(String param1, String param2) {
         CommunityFragment fragment = new CommunityFragment();
@@ -44,7 +48,7 @@ public class CommunityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_community, container, false);
+        final View view = inflater.inflate(R.layout.fragment_community, container, false);
 
         community_adapter = new CommunityListViewAdapter();
         community_listView = view.findViewById(R.id.listView);
@@ -58,13 +62,15 @@ public class CommunityFragment extends Fragment {
         filterBtn02_1 = view.findViewById(R.id.filterBtn02_1);
         filterBtn02_2 = view.findViewById(R.id.filterBtn02_2);
         filterBtn02_3 = view.findViewById(R.id.filterBtn02_3);
+        line01 = view.findViewById(R.id.line01);
+        line02 = view.findViewById(R.id.line02);
 
         backLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (filterBtn02.getVisibility() == View.VISIBLE) {
                     filterBtn01.setVisibility(View.VISIBLE);
-                    filterBtn02.setVisibility(View.GONE);
+                    setFilterBtn02_Gone();
                 }
             }
         });
@@ -73,35 +79,31 @@ public class CommunityFragment extends Fragment {
         filterBtn01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                filterBtn01.setVisibility(View.GONE);
-                filterBtn02.setVisibility(View.VISIBLE);
+                goToDown();
             }
         });
 
         filterBtn02_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                goToUp();
                 filterBtn01.setText("위시리스트 순");
-                filterBtn01.setVisibility(View.VISIBLE);
-                filterBtn02.setVisibility(View.GONE);
             }
         });
 
         filterBtn02_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                goToUp();
                 filterBtn01.setText("거리 순");
-                filterBtn01.setVisibility(View.VISIBLE);
-                filterBtn02.setVisibility(View.GONE);
             }
         });
 
         filterBtn02_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                goToUp();
                 filterBtn01.setText("카페 좋아요 순");
-                filterBtn01.setVisibility(View.VISIBLE);
-                filterBtn02.setVisibility(View.GONE);
             }
         });
         return view;
@@ -119,5 +121,58 @@ public class CommunityFragment extends Fragment {
             community_adapter.addItem(profile, name, dong, heart, place);
         }
         community_adapter.notifyDataSetChanged();
+    }
+
+    void setFilterBtn02_Gone() {
+        filterBtn02.setVisibility(View.GONE);
+        filterBtn02_1.setVisibility(View.GONE);
+        filterBtn02_2.setVisibility(View.GONE);
+        filterBtn02_3.setVisibility(View.GONE);
+        line01.setVisibility(View.GONE);
+        line02.setVisibility(View.GONE);
+
+        filterBtn01.setVisibility(View.VISIBLE);
+    }
+
+    void goToDown() {
+        filterBtn01.setVisibility(View.GONE);
+        filterBtn02.setVisibility(View.VISIBLE);
+        filterBtn02_1.setVisibility(View.VISIBLE);
+        filterBtn02_2.setVisibility(View.VISIBLE);
+        filterBtn02_3.setVisibility(View.VISIBLE);
+        line01.setVisibility(View.VISIBLE);
+        line02.setVisibility(View.VISIBLE);
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.up_down100);
+        line01.startAnimation(animation);
+        filterBtn02_2.startAnimation(animation);
+        animation = AnimationUtils.loadAnimation(getContext(), R.anim.up_down200);
+        line02.startAnimation(animation);
+        filterBtn02_3.startAnimation(animation);
+    }
+
+    void goToUp() {
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.down_up100);
+        line01.startAnimation(animation);
+        filterBtn02_2.startAnimation(animation);
+        animation = AnimationUtils.loadAnimation(getContext(), R.anim.down_up200);
+        line02.startAnimation(animation);
+        filterBtn02_3.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                filterBtn02.setVisibility(View.GONE);
+                filterBtn01.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
