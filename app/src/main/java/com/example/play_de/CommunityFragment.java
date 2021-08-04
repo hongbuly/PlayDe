358a316fad2d2771d15ba475bedd69d119df2dbc
hongbuly <60245930+hongbuly@users.noreply.github.com>
2021-08-05 00:36:08 +0900

    community list 삭제함. list의 layout을 변경하려는데 클릭한 list가 아닌 다른 list가 변경되고 블러처리에도 문제가 있어, scrollView로 대체함. 아직 blur처리가 이상함.

--- app/src/main/java/com/example/play_de/CommunityFragment.java
+++ app/src/main/java/com/example/play_de/CommunityFragment.java
@@ -1,35 +1,46 @@
 package com.example.play_de;
 
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.github.mmin18.widget.RealtimeBlurView;
+import com.google.android.material.imageview.ShapeableImageView;
 
 public class CommunityFragment extends Fragment implements OnBackPressedListener {
     private MainActivity main;
     private View view;
     private LinearLayout backLayout;
-    private ListView community_listView;
-    private CommunityListViewAdapter community_adapter;
     private EditText filterEdit;
     private Button filterBtn01;
     private RelativeLayout filterBtn02;
     private Button filterBtn02_1;
     private Button filterBtn02_2;
     private Button filterBtn02_3;
+
+    private HorizontalScrollView[] heart_list;
+    private ImageView[] profile;
+    private TextView[] name;
+    private TextView[] dong;
+    private TextView[] heart;
+    private ShapeableImageView[][] image;
+    private RealtimeBlurView blurView;
+    int clickNum = -1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,19 +53,7 @@ public class CommunityFragment extends Fragment implements OnBackPressedListener
         main = (MainActivity) getActivity();
         view = inflater.inflate(R.layout.fragment_community, container, false);
 
-        community_adapter = new CommunityListViewAdapter();
-        community_listView = view.findViewById(R.id.listView);
-        community_listView.setAdapter(community_adapter);
-        addCommunityListView();
-
-        filterEdit = view.findViewById(R.id.filterEdit);
-        filterBtn01 = view.findViewById(R.id.filterBtn01);
-        filterBtn02 = view.findViewById(R.id.filterBtn02);
-        filterBtn02_1 = view.findViewById(R.id.filterBtn02_1);
-        filterBtn02_2 = view.findViewById(R.id.filterBtn02_2);
-        filterBtn02_3 = view.findViewById(R.id.filterBtn02_3);
-
-        backLayout = view.findViewById(R.id.backLayout);
+        initialSetup();
         backLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -97,20 +96,6 @@ public class CommunityFragment extends Fragment implements OnBackPressedListener
             }
         });
         return view;
-    }
-
-    private void addCommunityListView() {
-        //프로필 이미지 타입 결정하고 설정할 것.
-        int profile = R.drawable.circle_grey;
-        String name = "이름";
-        String dong = "보드게임러버";
-        String heart = "5개가 겹칩니다.";
-        String place = "2개가 겹칩니다.";
-
-        for (int i = 1; i < 8; i++) {
-            community_adapter.addItem(profile, name, dong, heart, place);
-        }
-        community_adapter.notifyDataSetChanged();
     }
 
     private void setFilterBtn02_Gone() {
@@ -170,5 +155,87 @@ public class CommunityFragment extends Fragment implements OnBackPressedListener
     public void onResume() {
         super.onResume();
         main.setOnBackPressedListener(this, 2);
+    }
+
+    private void initialSetup() {
+        filterEdit = view.findViewById(R.id.filterEdit);
+        filterBtn01 = view.findViewById(R.id.filterBtn01);
+        filterBtn02 = view.findViewById(R.id.filterBtn02);
+        filterBtn02_1 = view.findViewById(R.id.filterBtn02_1);
+        filterBtn02_2 = view.findViewById(R.id.filterBtn02_2);
+        filterBtn02_3 = view.findViewById(R.id.filterBtn02_3);
+
+        backLayout = view.findViewById(R.id.backLayout);
+
+        heart_list = new HorizontalScrollView[7];
+        profile = new ImageView[7];
+        name = new TextView[7];
+        dong = new TextView[7];
+        heart = new TextView[7];
+        image = new ShapeableImageView[7][10];
+        blurView = view.findViewById(R.id.blurView);
+
+        Integer[] Rid_heartList = {R.id.heart_list01, R.id.heart_list02, R.id.heart_list03, R.id.heart_list04,
+                R.id.heart_list05, R.id.heart_list06, R.id.heart_list07};
+        Integer[] Rid_profile = {R.id.profile01, R.id.profile02, R.id.profile03, R.id.profile04,
+                R.id.profile05, R.id.profile06, R.id.profile07};
+        Integer[] Rid_name = {R.id.name01, R.id.name02, R.id.name03, R.id.name04,
+                R.id.name05, R.id.name06, R.id.name07};
+        Integer[] Rid_dong = {R.id.dong01, R.id.dong02, R.id.dong03, R.id.dong04,
+                R.id.dong05, R.id.dong06, R.id.dong07};
+        Integer[] Rid_heart = {R.id.heart01, R.id.heart02, R.id.heart03, R.id.heart04,
+                R.id.heart05, R.id.heart06, R.id.heart07};
+        Integer[][] Rid_image = {{R.id.image01_1, R.id.image01_2, R.id.image01_3, R.id.image01_4,
+                R.id.image01_5, R.id.image01_6, R.id.image01_7, R.id.image01_8, R.id.image01_9, R.id.image01_10},
+                {R.id.image02_1, R.id.image02_2, R.id.image02_3, R.id.image02_4,
+                        R.id.image02_5, R.id.image02_6, R.id.image02_7, R.id.image02_8, R.id.image02_9, R.id.image02_10},
+                {R.id.image03_1, R.id.image03_2, R.id.image03_3, R.id.image03_4,
+                        R.id.image03_5, R.id.image03_6, R.id.image03_7, R.id.image03_8, R.id.image03_9, R.id.image03_10},
+                {R.id.image04_1, R.id.image04_2, R.id.image04_3, R.id.image04_4,
+                        R.id.image04_5, R.id.image04_6, R.id.image04_7, R.id.image04_8, R.id.image04_9, R.id.image04_10},
+                {R.id.image05_1, R.id.image05_2, R.id.image05_3, R.id.image05_4,
+                        R.id.image05_5, R.id.image05_6, R.id.image05_7, R.id.image05_8, R.id.image05_9, R.id.image05_10},
+                {R.id.image06_1, R.id.image06_2, R.id.image06_3, R.id.image06_4,
+                        R.id.image06_5, R.id.image06_6, R.id.image06_7, R.id.image06_8, R.id.image06_9, R.id.image06_10},
+                {R.id.image07_1, R.id.image07_2, R.id.image07_3, R.id.image07_4,
+                        R.id.image07_5, R.id.image07_6, R.id.image07_7, R.id.image07_8, R.id.image07_9, R.id.image07_10}};
+
+        for (int i = 0; i < 7; i++) {
+            final int finalI1 = i;
+            heart_list[i] = view.findViewById(Rid_heartList[i]);
+            profile[i] = view.findViewById(Rid_profile[i]);
+            name[i] = view.findViewById(Rid_name[i]);
+            dong[i] = view.findViewById(Rid_dong[i]);
+            heart[i] = view.findViewById(Rid_heart[i]);
+            heart[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (clickNum == -1) {
+                        clickNum = finalI1;
+                        heart_list[finalI1].setVisibility(View.VISIBLE);
+                        blurView.setVisibility(View.VISIBLE);
+                        filterBtn01.setVisibility(View.GONE);
+                    } else {
+                        heart_list[clickNum].setVisibility(View.GONE);
+                        clickNum = finalI1;
+                        heart_list[finalI1].setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+
+            for (int j = 0; j < 10; j++) {
+                image[i][j] = view.findViewById(Rid_image[i][j]);
+            }
+        }
+
+        blurView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                heart_list[clickNum].setVisibility(View.GONE);
+                blurView.setVisibility(View.GONE);
+                filterBtn01.setVisibility(View.VISIBLE);
+                clickNum = -1;
+            }
+        });
     }
 }
