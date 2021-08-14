2a25cd5996d770149123665d47c5688795e0ed48
hongbuly <60245930+hongbuly@users.noreply.github.com>
2021-08-14 13:26:50 +0900

    community recycler, tag recycler 추가. tag 는 클릭했을 때, 배경만 바뀌게 만듬. 다른 이벤트는 없음.

--- app/src/main/java/com/example/play_de/CommunityFragment.java
+++ app/src/main/java/com/example/play_de/CommunityFragment.java
@@ -18,14 +18,20 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+
 public class CommunityFragment extends Fragment implements OnBackPressedListener {
     private MainActivity main;
     private View view;
     private EditText filterEdit;
 
+    private RecyclerView tag_recyclerView;
+    private CommunityTagAdapter communityTagAdapter;
+    private RecyclerView.LayoutManager tagLayoutManager;
+
     private RecyclerView community_recyclerView;
     private CommunityRecyclerAdapter communityRecyclerAdapter;
-    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.LayoutManager communityLayoutManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,12 +52,35 @@ public class CommunityFragment extends Fragment implements OnBackPressedListener
     private void initialSetUp() {
         filterEdit = view.findViewById(R.id.filterEdit);
 
+        communityTagAdapter = new CommunityTagAdapter();
+        tag_recyclerView = view.findViewById(R.id.tag_recycler);
+        tagLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        tag_recyclerView.setLayoutManager(tagLayoutManager);
+        tag_recyclerView.setAdapter(communityTagAdapter);
+        addTagRecyclerView();
+
         communityRecyclerAdapter = new CommunityRecyclerAdapter();
         community_recyclerView = view.findViewById(R.id.community_recycler);
-        layoutManager = new LinearLayoutManager(getActivity());
-        community_recyclerView.setLayoutManager(layoutManager);
+        communityLayoutManager = new LinearLayoutManager(getActivity());
+        community_recyclerView.setLayoutManager(communityLayoutManager);
         community_recyclerView.setAdapter(communityRecyclerAdapter);
         addCommunityRecyclerView();
+    }
+
+    private void addTagRecyclerView() {
+        String item = "#추천해요";
+        communityTagAdapter.addItem(item);
+        item = "#만나요";
+        communityTagAdapter.addItem(item);
+        item = "#질문있어요";
+        communityTagAdapter.addItem(item);
+        item = "#보드게임 소식";
+        communityTagAdapter.addItem(item);
+        item = "#일상";
+        communityTagAdapter.addItem(item);
+        item = "#수다";
+        communityTagAdapter.addItem(item);
+        communityTagAdapter.notifyDataSetChanged();
     }
 
     private void addCommunityRecyclerView() {
@@ -68,6 +97,12 @@ public class CommunityFragment extends Fragment implements OnBackPressedListener
         item = new CommunityRecycler();
         name = "박혜원";
         content = "루미큐브 앱으로도 잘 되어 있던데, 혹시 비대면으로 지금 4인으로 보드 게임하실 분들 계신가요?";
+        item.setData(image, name, level, content);
+        communityRecyclerAdapter.addItem(item);
+
+        item = new CommunityRecycler();
+        name = "이지상";
+        content = "Ticket To Ride 모바일 앱이 있던데, 같이 하실 분 있나요??";
         item.setData(image, name, level, content);
         communityRecyclerAdapter.addItem(item);
         communityRecyclerAdapter.notifyDataSetChanged();
